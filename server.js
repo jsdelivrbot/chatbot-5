e57e1c6f5dac5dc7ec1efd6ce76e61521ec19c3c
jewkub <jew.napat@hotmail.com>
2017-08-26 14:54:42 +0700

    basic

--- server.js
+++ server.js
@@ -1,18 +1,30 @@
-var express = require('express');
-var app = express();
-
-app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
+let express = require('express');
+let app = express();
+let firebase = require('firebase');
+firebase.initializeApp({
+  apiKey: "AIzaSyBzGpgnK1Uh-sYTTfLbcxOg585OHyLRrfE",
+  authDomain: "chatbot-2c589.firebaseapp.com",
+  databaseURL: "https://chatbot-2c589.firebaseio.com",
+  projectId: "chatbot-2c589",
+  storageBucket: "chatbot-2c589.appspot.com",
+  messagingSenderId: "209612491552"
 });
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+let db = firebase.database();
+let login = require("facebook-chat-api");
+let matching = {};
+db.ref('/matching').once('value').then((data) => {
+  data = data.val();
+  data.forEach((e, i, arr) => {
+    matching[e.a] = e.b;
+    matching[e.b] = e.a;
+  });
+});
+login({
+  email: "clbotchat@gmail.com", password: "clbotchat"
+}, (err, api) => {
+  if(err) console.error(err);
+  api.listen((err, message) => {
+    if(err) console.error(err);
+    if(matching[message.senderID]) api.sendMessage(message.body, matching[message.senderID]);
+  });
 });
